ede94cfaeb02de608d9fce93de61144d8051f98f
Lily192021 <80777955+Lily192021@users.noreply.github.com>
2022-03-30 13:52:51 -0600

    Add files via upload

---
+++ CRUDLAB1/app/Http/Controllers/ProyectoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proyecto;
+use Illuminate\Http\Request;
+
+class ProyectoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data['proyectos']=Proyecto::paginate(5);
+        return view('proyecto.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('proyecto.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //todos los datos que se envien se van a recepcionar
+        $proyectoData = request()->except('_token');
+        Proyecto::insert($proyectoData);
+        return response()->json($proyectoData); //vamos a retornar los datos que han sido enviados
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        //recuperar los datos
+        $proyecto=Proyecto::findOrFail($id);
+        return view('proyecto.edit', compact('proyecto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
